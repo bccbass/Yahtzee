d43d65ab855543f187bfb8d85e249d46535b294a
Benjamin Campbell <benjamincampbell@benjamins-mini.lan>
2023-05-05 15:39:04 +1000

    finish simple dice funcs. refactor to Dice class

---
+++ Dice-class.py
@@ -0,0 +1,28 @@
+from random import randint
+
+class Dice:
+    @classmethod
+    def die(cls, ):
+        return randint(1, 6)
+    @classmethod
+    def roll(cls, num_dice):
+        hand_cache = []
+        for _ in range(num_dice):
+            hand_cache.append(cls.die())
+        return hand_cache
+    @classmethod
+    def remove(cls, bad_dice, hand):
+        for die in bad_dice:
+            hand.remove(die)
+        return hand
+
+    @classmethod
+    def re_roll(cls, keeper_hand):
+        re_roll_x = 5 - len(keeper_hand)
+        new_dice = cls.roll(re_roll_x)
+        return keeper_hand + new_dice
+
+print(Dice.die())
+print(Dice.roll(5))
+print(Dice.remove([1,2,3], [1,2,3,4,5,6]))
+print(Dice.re_roll([1]))
--- funcs.py
+++ funcs.py
@@ -5,9 +5,8 @@ def die():
 
 def roll_dice(num_dice):
     hand_cache = []
-    for die in range(num_dice):
-        x = die()
-        hand_cache.append(x)
+    for _ in range(num_dice):
+        hand_cache.append(die())
     return hand_cache
 
 def remove_dice_fr_hand(bad_dice, hand):
@@ -17,23 +16,17 @@ def remove_dice_fr_hand(bad_dice, hand):
 
 
 def re_roll_dice(keeper_hand):
-    re_roll_x_times = 5 - len(keeper_hand)
-    for i in range(re_roll_x_times):
-        keeper_hand.append(keeper_hand)
-    # re_roll = roll_dice(re_roll_x_times)
-    # for die in re_roll:
-    #     keeper_hand.append(die)
-    return keeper_hand
+    re_roll_x = 5 - len(keeper_hand)
+    new_dice = roll_dice(re_roll_x)
+    return keeper_hand + new_dice
 
 
-
-print(die())
-print(roll_dice(5))
 
 # hand = roll_dice(5)
 # reroll = re_roll_dice([1,2,3])
 
-# print(remove_dice_fr_hand([1,2,3], [1,2,3,4,5,6]))
+print(remove_dice_fr_hand([1,2,3], [1,2,3,4,5,6]))
+print('reroll: ', re_roll_dice([1]))
 # print('reroll-> ', reroll)
 # print('Yahtzee hand -> ', hand)
 
