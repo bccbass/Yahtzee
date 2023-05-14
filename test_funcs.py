e1aaa73b3753af65502ea4406edbdbc2fa3e6515
bccbass <109984363+bccbass@users.noreply.github.com>
2023-05-14 16:33:15 +1000

    sync

--- test_funcs.py
+++ test_funcs.py
@@ -1,6 +1,6 @@
 import pytest
 
-from funcs import is_valid_die, input_filter
+from funcs import is_valid_die, input_filter, remove_prompt
 
 def test_is_valid_die():
     assert is_valid_die('12345')
@@ -21,6 +21,9 @@ def test_input_filter():
     assert input_filter('quit') == 'q'
     assert not input_filter('q') == False
     assert input_filter('wrong input') == False
-    
-    
-    assert not input_filter('rffasldkf')
+
+
+
+ 
+
+
